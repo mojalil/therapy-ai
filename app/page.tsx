9a16d93b65950eed2c5070a0396da02ee11b04e6
amanda <achow3012@gmail.com>
2023-08-31 02:48:25 +0800

    added and edited next-template

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
-export default function Home(){
-  return <div>
-  <header>
-    <h1>Theraphy AI</h1>
-  </header> </div>
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Therapy AI
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+        Therapy AI is a mental health and well-being app designed to provide users with a simulated therapy experience. Through our platform, users can book and take appointments with AI-driven therapy agents that replicate the experience of a real therapist.
+        </p>
+      </div>
+    </section>
+  )
 }
---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Therapy AI",
+  description:
+    "Therapy AI is a mental health and well-being app designed to provide users with a simulated therapy experience. Through our platform, users can book and take appointments with AI-driven therapy agents that replicate the experience of a real therapist.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
