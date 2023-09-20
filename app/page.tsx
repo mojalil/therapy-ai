10974ee5e019ac9bda6f5e5c2d4f215ce2cc42cc
Amanda <achow3012@gmail.com>
2023-09-20 17:30:49 +0800

    add list of therapists for users to choose from

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import Therapist from "@/components/therapists"
 
 export default function IndexPage() {
   return (
@@ -10,10 +11,17 @@ export default function IndexPage() {
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Therapy AI
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
+        <p className="text-lg text-muted-foreground">
         Therapy AI is a mental health and well-being app designed to provide users with a simulated therapy experience. Through our platform, users can book and take appointments with AI-driven therapy agents that replicate the experience of a real therapist.
         </p>
       </div>
+      <div>
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">Choose your Therapist</h1>
+      </div>
+      <div className="flex items-center">
+      <Therapist therapistImage="/therapist_1.png"/> <Therapist therapistImage="/therapist_2.png" />
+      </div>
+
     </section>
   )
 }
