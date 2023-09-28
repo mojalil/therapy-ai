64fa3eb981d43442331fd41869012d040d19f57f
motypes <mohammed.jalil@gmail.com>
2023-09-28 09:39:31 +0800

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
 import Therapist from "@/components/therapists"
 
 export default function IndexPage() {
